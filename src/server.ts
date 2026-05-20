9b1a49dac73b8b037220653043d3955d91d439b3
Lucas Leung <kayauleung2024@gmail.com>
2026-05-20 18:30:10 +0800

    minify in wrangler

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import { Elysia, status, t } from "elysia";
 import { Service } from "./service";
 import { CloudflareAdapter } from "elysia/adapter/cloudflare-worker";
 import { env } from "cloudflare:workers";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia({ adapter: CloudflareAdapter, aot: true })
+  .use(cors({ origin: "https://hoppscotch.io", allowedHeaders: ["Content-Type"] }))
   .post(
     "/stockPrice",
     async ({ body }) => {
